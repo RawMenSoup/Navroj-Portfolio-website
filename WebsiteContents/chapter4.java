06340db8b15a0ff4cb7d1909dec69cff3eb8f82b
Navroj Billing <billingnavroj@gmail.com>
2026-01-29 23:32:27 -0800

    Add Pen Project showcase and dedicated project page

---
+++ WebsiteContents/chapter4.java
@@ -0,0 +1,10 @@
+public class chapter4 {
+  public static double fractionSum(int n) {
+    double sum = 0.0;
+    for (int i = 1; i <= n; i++) {
+        sum += 1.0 / i;
+    }
+    return sum;
+}
+
+}
